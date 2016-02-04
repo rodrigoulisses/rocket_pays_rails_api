2a65af0edd25945bd9609697b1c405baac60b7d2
Rodrigo Ulisses e Silva <rodrigo.ulisses7@gmail.com>
2016-02-03 23:10:58 -0300

    Add gem codeclimate-test-reporter

--- Gemfile
+++ Gemfile
@@ -5,4 +5,5 @@ gemspec
 
 group :test do
   gem "shoulda-matchers"
+  gem 'codeclimate-test-reporter', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,5 @@ require 'shoulda/matchers'
 require 'rocket_pays_rails_api'
 
 require 'support/shoulda_matchers'
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
