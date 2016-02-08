f1abc611c6af845cdd069e9fc33744f35a02cf09
Rodrigo Ulisses e Silva <rodrigo.ulisses7@gmail.com>
2016-02-08 16:37:05 -0300

    Refectoring method Post in class Request
    Return json the request.

--- lib/rocket_pays_rails_api/request.rb
+++ lib/rocket_pays_rails_api/request.rb
@@ -1,33 +1,38 @@
 module RocketPaysRailsApi
   class Request < Base
     # Set link
-    attr_accessor :link
+    attr_accessor :resource
 
     # Set params
     attr_accessor :params
 
-    # return redirect url to ewallet
+    # return json
     def self.post!(options = {})
       self.new(options).post
     end
 
     def post
-      url = URI.parse("#{RocketPaysRailsApi.link}/#{self.link}")
-
       request = Net::HTTP::Post.new(url.path)
       request.basic_auth RocketPaysRailsApi.email, RocketPaysRailsApi.token
       request.body = self.params
       request.content_type = "application/json"
 
-      response = Net::HTTP.start(url.host, url.port, use_ssl: use_ssl?) { |http| http.request(request) }
 
-      return JSON.parse(response.body)["retirectUrl"]
+      return JSON.parse(http_start(request).body)
     end
 
     private
-    
+
+    def url
+      URI.parse("#{RocketPaysRailsApi.link}/#{self.resource}")
+    end
+
     def use_ssl?
       RocketPaysRailsApi.production?
+    end
+
+    def http_start(request)
+      Net::HTTP.start(url.host, url.port, use_ssl: use_ssl?) { |http| http.request(request) }
     end
   end
 end
